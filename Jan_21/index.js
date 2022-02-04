32ef95983f0c4920cd2a6fabfd868a95fc5dcd99
anuammishra <75531435+anuammishra@users.noreply.github.com>
2022-02-04 22:49:25 +0530

    Update index.js

--- Jan_21/index.js
+++ Jan_21/index.js
@@ -1,6 +1,6 @@
  var img_arr = 
     [
-      'https://www.pexels.com/photo/photo-of-parked-black-and-red-triumph-daytona-sports-bike-3660527/' ,
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fnature%2F&psig=AOvVaw2p1M0P0qkp4VHsSbAIl0KB&ust=1644081513716000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPC694fH5vUCFQAAAAAdAAAAABAD' ,
       'https://www.pexels.com/photo/string-lights-hanged-on-bed-frame-1329711/',
       'https://www.pexels.com/photo/white-sports-coupe-733745/' ,
       'https://images.unsplash.com/photo-1623063921261-5a9914c19548?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fHJhbmRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' ,
